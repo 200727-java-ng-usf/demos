fe723615d32ec3753a7edb4ff860b06b46e0ef5a
Stephen Brasel <wearyroad@gmail.com>
2020-07-28 17:41:54 -0400

    added static blocks

--- stephen_brasel_code/Java/FirstClass.java
+++ stephen_brasel_code/Java/FirstClass.java
@@ -2,6 +2,28 @@ public class FirstClass{
 	public static void main(String[] args){
 		System.out.println("Hello, future Java devs!");
 	}
+
+	public FirstClass(){
+		super();
+		
+	}
+
+	static{
+		System.out.println("I run, but when?");
+		// OUTER:
+		// for(int i = 0; i < 4; i++){
+		// 	for(int j = 0; j < 4; j++){
+		// 		if(i == 1){
+		// 			break OUTER;
+		// 		}
+		// 		System.out.println(":beer:!");
+		// 	}
+		// }
+	}
+
+	{
+		System.out.println("Do I run?");
+	}
 }
 
 // class AnotherClass{
