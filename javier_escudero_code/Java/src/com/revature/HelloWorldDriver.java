63ab5e65e7b92be120840ffcf5068a78b2125b66
escuderj <javierusn@yahoo.com>
2020-07-29 12:35:07 -0400

    updated gitignore, added main to driver

--- javier_escudero_code/Java/src/com/revature/HelloWorldDriver.java
+++ javier_escudero_code/Java/src/com/revature/HelloWorldDriver.java
@@ -1,5 +1,7 @@
 package com.revature;
 
 public class HelloWorldDriver {
-
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Development with an IDE");
+    }
 }
