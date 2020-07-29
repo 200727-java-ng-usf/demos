4edd40ec434e74ed8a54495297357c4f31645852
capolimia <capolimia@gmail.com>
2020-07-29 17:15:58 -0400

    Working on q1 homework

--- mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
+++ mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
@@ -1,14 +1,17 @@
 package com.revature;
 
 import com.revature.arrays.ArrayDemo;
+import com.revature.question1Test.Question1;
 
 public class HelloWorldDriver {
 
     public static void  main(String[] args) {
         System.out.println("Hello wurld, welcome to Java Development with an IDE!");
 
-        ArrayDemo arrayDemo = new ArrayDemo();
-        arrayDemo.learningArrays();
+//      ArrayDemo arrayDemo = new ArrayDemo();
+//      arrayDemo.learningArrays();
+        Question1 q1 = new Question1();
+        q1.bubbleSort();
     }
 
 }
---
+++ mia_capoli_code/Java/day3-Demos/src/com/revature/question1Test/Question1.java
@@ -0,0 +1,26 @@
+package com.revature.question1Test;
+
+import java.util.Arrays;
+
+public class Question1 {
+
+    private void printValues(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+    public void bubbleSort() {
+
+        int[] q1Array = {1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4};
+
+        for (int i = 0; i < q1Array.length; i++) {
+            System.out.println(q1Array[i]);
+        }
+
+
+    }
+
+}
