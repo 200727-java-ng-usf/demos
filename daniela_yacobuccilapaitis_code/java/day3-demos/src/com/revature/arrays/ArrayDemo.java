28baa420848fc0909e10c0a90a4b611f88124282
Daniela Yacobucci-Lapaitis <dyacobucci47@gmail.com>
2020-07-29 15:33:26 -0400

    wrote array demo notes

---
+++ daniela_yacobuccilapaitis_code/java/day3-demos/src/com/revature/arrays/ArrayDemo.java
@@ -0,0 +1,25 @@
+package com.revature.arrays;
+
+
+public class ArrayDemo {
+
+    public void learningArrays() {
+
+        /*
+                Array declarations
+
+                    int[] arr = new int[5];
+                    int []arr = new int[5];
+                    int arr[] = new int[5];
+
+                Arrays are of a fixed size, meaning that once
+                initialized (created_ their size will never change
+         */
+        int[] intArray = new int[5];
+
+        System.out.println(intArray);
+
+
+    }
+
+}
