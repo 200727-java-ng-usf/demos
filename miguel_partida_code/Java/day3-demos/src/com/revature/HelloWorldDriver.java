4d311c6e80b2d3c655722c466058cd65adcd220b
Miguel Partida <mig21.ap@gmail.com>
2020-07-29 11:32:04 -0700

    reworked the structure of the program

---
+++ miguel_partida_code/Java/day3-demos/src/com/revature/HelloWorldDriver.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class HelloWorldDriver {
+
+    public static void main(String [] args){
+    System.out.println("Hello!");
+    }
+}
--- miguel_partida_code/src/com/revature/HelloWorldDriver.java
+++
@@ -1,9 +0,0 @@
-package com.revature;
-
-public class HelloWorldDriver {
-
-    public static void main(String [] args) {
-        System.out.println("Welcome to Java development with an IDE!");
-    }
-
-}
