22c52829572fff3fb82a90cc7fcb7813e8525b86
capolimia <capolimia@gmail.com>
2020-07-29 12:44:48 -0400

    line modification for helloworld program

--- mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
+++ mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
@@ -3,7 +3,7 @@ package com.revature;
 public class HelloWorldDriver {
 
     public static void  main(String[] args) {
-        System.out.println("Hello wurld, welcome to java Development with an IDE!");
+        System.out.println("Hello wurld, welcome to Java Development with an IDE!");
     }
 
 }
