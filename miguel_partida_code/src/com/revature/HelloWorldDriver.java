661dd2a6d975c65f65a803894985a09799ae8028
Miguel Partida <mig21.ap@gmail.com>
2020-07-29 09:41:07 -0700

    updated gitignore, updated main on Driver

--- miguel_partida_code/src/com/revature/HelloWorldDriver.java
+++ miguel_partida_code/src/com/revature/HelloWorldDriver.java
@@ -2,4 +2,8 @@ package com.revature;
 
 public class HelloWorldDriver {
 
+    public static void main(String [] args) {
+        System.out.println("Welcome to Java development with an IDE!");
+    }
+
 }
