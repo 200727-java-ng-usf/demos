4db214717494353e84d86ee5885c3c492ae85cb3
cornelldixon <cornell.dixon.cd@gmail.com>
2020-07-29 11:34:30 -0500

    updated gitignore, added main to driver

---
+++ Java/cornell_dixon_code/day3-demos_Dixon/src/com/revature/HelloWorldDriver.java
@@ -0,0 +1,9 @@
+package com.revature;
+
+public class HelloWorldDriver {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java development with an IDE");
+
+    }
+}
