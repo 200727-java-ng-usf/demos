40aa6a2e653af652162192755db35852b3583fb7
wezley <wezley.singleton@gmail.com>
2020-07-29 14:30:01 -0400

    added comment

--- java/day3-demos/src/com/revature/HelloWorldDriver.java
+++ java/day3-demos/src/com/revature/HelloWorldDriver.java
@@ -3,6 +3,7 @@ package com.revature;
 public class HelloWorldDriver {
 
     public static void main(String[] args) {
+        // Adding a comment to commit
         System.out.println("Welcome to Java development with an IDE!");
     }
 
