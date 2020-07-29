5dae3134f93f3c80d03edb5ae29b10df5a23bf96
Katie Gray <katiegray@Katies-MacBook-Pro-2.local>
2020-07-29 14:40:51 -0500

    ArrayDemo more Arrays

--- katie_gray_code/java/day3Demos/src/com/revature/arrays/ArrayDemo.java
+++ katie_gray_code/java/day3Demos/src/com/revature/arrays/ArrayDemo.java
@@ -1,6 +1,8 @@
 package com.revature.arrays;
 
 
+import java.util.Arrays;
+
 public class ArrayDemo {
 
     public void leaningArrays(){
@@ -29,11 +31,24 @@ public class ArrayDemo {
 
         printValues(myStrings);
         printValues(intArray);
+
+        int[] otherints = Arrays.copyOf(intArray, intArray.length+1);
+        System.out.println("intArray length: "+ intArray.length);
+        System.out.println("otherInts length: "+ otherints.length);
+
+        //unboxing
+        int[] arr = {new Integer(4)};
+        //autoboxing
+        Integer[] integerArray = { 123 };
+
+        // you want a heterogeneous array (with different types, you need to declare a higher level object like Object
+        Object[] randomArray = { 12, "test", false, "4.3"};
+
     }
 
     //method overloading, these are methods that have the same name but
     //have different data types or parameters and number of arguments
-    //also known as compile-type polymorphism
+    //also known as compile-time polymorphism
 
     private void printValues(int[] array){
         for( int i = 0; i < array.length; i++){
@@ -45,4 +60,16 @@ public class ArrayDemo {
             System.out.println(arr[i]);
         }
     }
+    //we can reduce the redundancy of having two methods that do the same thing
+    //by making out printValues method more "generic"
+    //does not work in the cases of primitive type arrays (ex: int[] cannot be converted to Object[]
+
+//    private void printValues(Object[] arr){
+//        for( int i = 0; i < arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+//    }
 }
+
+//arrays are of a fixed size and they cannot be changed but they can be copied into a new array
+//the arrays class provided by the JRE is a utility class for doing such an operation
