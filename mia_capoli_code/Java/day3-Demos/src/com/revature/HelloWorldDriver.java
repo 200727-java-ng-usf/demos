4ef5c63909cd8f6ffa645821fa791be319875761
capolimia <capolimia@gmail.com>
2020-07-29 12:32:43 -0400

    edit gitignore to add out directory

--- mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
+++ mia_capoli_code/Java/day3-Demos/src/com/revature/HelloWorldDriver.java
@@ -1,4 +1,9 @@
 package com.revature;
 
 public class HelloWorldDriver {
+
+    public static void  main(String[] args) {
+        System.out.println("Hello wurld, welcome to java Development with an IDE!");
+    }
+
 }
