5fbf52275e2b489c2da6da6a467431c2190a12d1
YuanfnegGu <yuanfenggu@hotmail.com>
2020-07-29 19:54:14 -0500

    Maven JUnit test

--- MavenDemo/src/main/java/com/revature/MavenTest.java
+++ MavenDemo/src/main/java/com/revature/MavenTest.java
@@ -3,5 +3,10 @@ package com.revature;
 public class MavenTest {
     public static void main(String[] args) {
         System.out.println("first maven project");
+
+        Question1 message1 = new Question1("yuanfeng");
+        System.out.println(message1.toString());
+
+
     }
 }
