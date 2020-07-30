2783cbb530443c5b106efafa9c83b839c624a016
Anthony Botello <help>
2020-07-30 14:48:20 -0700

    added OOP stuff to demo

--- anthony_botello_code/java/day4-demos/src/main/java/com/revature/App.java
+++ anthony_botello_code/java/day4-demos/src/main/java/com/revature/App.java
@@ -53,10 +53,28 @@ public class App {
         final Person p1 = new Person("Bob", "Bailey");  
         p1.setFirstName("Bill");
         System.out.println(p1);
-
+        
         // p1 = new Person("Doesn't", "Work"); // won't work
 
+        System.out.println("+-----------------------------------------------------+");
+        // Polymorphism
 
+        Object o = new Person("Howard", "Hughes");  // covariance
+        
+        // Person p = new Object();    // contravariance (not allowed)
+
+        ((Person) o).getFirstName();
+        
+        // o.setFirstName("H");    // won't work, Object behavior not determined until runtime
+
+        int i = 10;
+        long l = i; // int widened to long
+        System.out.println(l);
+
+        long l2 = 3_000_000_000L;
+        // int i2 = l2;    // can't narrow int to long
+        int i2 = (int) l2;  // explicitly cast, but risk of overflow
+        System.out.println(i2);
 
     }
 }
