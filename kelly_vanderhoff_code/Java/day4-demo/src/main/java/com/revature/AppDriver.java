cc2d9ce1bdee55d487e557288fdd80b45129cb0f
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-07-30 10:52:28 -0500

    Varargs demo

---
+++ kelly_vanderhoff_code/Java/day4-demo/src/main/java/com/revature/AppDriver.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+import com.revature.varargs.AppUser;
+import com.revature.varargs.Role;
+
+public class AppDriver {
+
+    // You can legally use variable arguments with the main method signature:
+    //      public static void main(String... args)
+    // but it is not common/conventional
+    public static void main(String[] args) {
+
+        AppUser user = new AppUser("msue", "passme", new Role[] { Role.TRAINER });
+        // variable arguments allows us to pass in a varying number of arguments to the addRoles method
+        user.addRoles(Role.DEV, Role.BLDG_MNGR);
+        System.out.println(user);
+
+    }
+}
