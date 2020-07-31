7b511cf6618bcd4964b295952f393b454138c75f
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-07-31 10:12:33 -0700

    added covariance example

--- jason_hernandez_code/java/day-5-demos/src/main/java/com/revature/abstractions/AbstractDiver.java
+++ jason_hernandez_code/java/day-5-demos/src/main/java/com/revature/abstractions/AbstractDiver.java
@@ -6,5 +6,13 @@ public class AbstractDiver {
         Cat myCat = new Cat();
         Cat yourCat = new Cat(9, "Siamese", true);
         Cat hisCat = new Cat(8, "Sphinx", false);
+
+        System.out.println("+---------------------+");
+        System.out.println(myCat.getBreed());
+        System.out.println("+---------------------+");
+
+        Animal someAnimal = new Cat(); // Covariance
+        System.out.println(someAnimal.numberOfLives); // 1 - it uses Animal state
+        System.out.println(someAnimal.getNumberOfLives()); // 9 - it gets the Cat state by the override and this
     }
 }
