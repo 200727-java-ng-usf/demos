8db05966007c4f9043dc24c677663a5b57bd2136
Anthony Botello <help>
2020-07-31 15:45:10 -0700

    end of day commit

--- anthony_botello_code/java/day5-demos/src/test/java/com/revature/CrocodilianTest.java
+++ anthony_botello_code/java/day5-demos/src/test/java/com/revature/CrocodilianTest.java
@@ -4,17 +4,44 @@ import static org.junit.Assert.assertEquals;
 
 import com.revature.abstractions.Crocodilian;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CrocodilianTest {
     
     private Crocodilian croc = new Crocodilian();
-
+    
+    @Before
+    public void setup(){
+        System.out.println("before");
+    }
+    
+    @After
+    public void after(){
+        System.out.println("after");
+    }
+    
+    @BeforeClass
+    public static void beforeAll(){
+        System.out.println("before everything");
+    }
+    
+    @AfterClass
+    public static void afterAll(){
+        System.out.println("after everything");
+    }
+    
     @Test
-    public void Test(){
+    public void test(){
         int original = croc.getNumberOfLives();
         croc.makeSound();
         assertEquals(croc.getNumberOfLives(), original + 1);
     }
 
+    public void test2(){
+        assertEquals("Alligator", croc.getSpecies());
+    }
 }
