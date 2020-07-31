8d9a9d0c0c7e06a6d2cd8fec24cf1886ed7149f2
wezley <wezley.singleton@gmail.com>
2020-07-31 11:06:32 -0400

    pushing first part of abstract demo

---
+++ java/day5-demos/src/main/java/com/revature/abstractions/Cat.java
@@ -0,0 +1,55 @@
+package com.revature.abstractions;
+
+public class Cat extends Animal {
+
+    private int numberOfLives; // Animal.numberOfLives is shadowed by this declaration
+    private String breed;
+    private boolean hasFur;
+
+    public Cat() {
+        // a call to super() is implicit, no need to actually type it out
+        super(); // invocation of the super class's no-args constructor
+        System.out.println("Cat no-args constructor called!");
+        this.numberOfLives = 9;
+        this.breed = "Domestic shorthair";
+        this.hasFur = true;
+    }
+
+    public Cat(int numberOfLives, String breed, boolean hasFur) {
+        System.out.println("Cat parameterized constructor called!");
+        this.numberOfLives = numberOfLives;
+        this.breed = breed;
+        this.hasFur = hasFur;
+    }
+
+    @Override // overridden because Animal also declares a method with the same name
+    public int getNumberOfLives() {
+        return numberOfLives;
+    }
+
+    public void setNumberOfLives(int numberOfLives) {
+        this.numberOfLives = numberOfLives;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow.");
+    }
+
+}
