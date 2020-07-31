5b71583f417ac9d825c79e81b894cfaaae448cde
YuanfnegGu <yuanfenggu@hotmail.com>
2020-07-30 20:10:14 -0500

    add Integer.paresInt

--- MavenDemo/src/main/java/com/revature/MavenTest.java
+++ MavenDemo/src/main/java/com/revature/MavenTest.java
@@ -4,8 +4,12 @@ public class MavenTest {
     public static void main(String[] args) {
         System.out.println("first maven project");
 
-        Question1 message1 = new Question1("yuanfeng");
+        Question1 message1 = new Question1("123");
         System.out.println(message1.toString());
+
+        int i1 = Integer.parseInt(message1.getName());
+
+
 
 
     }
