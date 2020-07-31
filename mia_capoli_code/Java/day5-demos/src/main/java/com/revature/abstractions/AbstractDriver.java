792fe64d1ac799f7b19384c22d96742ee22d7953
capolimia <capolimia@gmail.com>
2020-07-31 13:11:58 -0400

    finishing abstract class demo

--- mia_capoli_code/Java/day5-demos/src/main/java/com/revature/abstractions/AbstractDriver.java
+++ mia_capoli_code/Java/day5-demos/src/main/java/com/revature/abstractions/AbstractDriver.java
@@ -12,9 +12,30 @@ public class AbstractDriver {
         Cat hisCat = new Cat(5, "Persian", true);
         Cat herCat = new Cat(7, "Serval", true);
 
-        Animal someAnimal = new Cat(); // covariance!
+        System.out.println("+-----------");
+
+
+
+        System.out.println("+-----------");
 
         System.out.println(myCat.getBreed());
+        System.out.println(yourCat.isHasFur());
+        System.out.println(hisCat.isHasFur());
+        herCat.makeSound();
+
+        System.out.println("+-----------");
+
+
+        Animal someAnimal = new Cat(); // covariance!
+        /*
+            Covariance is when you have a reference of a super type that points to an object ... If methods are overridden
+         */
+
+        System.out.println(someAnimal.numberOfLives); // 1
+        System.out.println(someAnimal.getNumberOfLives());
+        Cat someCat = (Cat) someAnimal;
+        System.out.println(someCat.getAnimalNumberOfLives());
+
 
     }
 }
--- mia_capoli_code/Java/day5-demos/src/main/java/com/revature/abstractions/Cat.java
+++ mia_capoli_code/Java/day5-demos/src/main/java/com/revature/abstractions/Cat.java
@@ -16,16 +16,24 @@ public class Cat extends Animal {
     }
 
     public Cat(int numberOfLives, String breed, boolean hasFur) {
+        super();
+        // this(); cannot include both super and this bc they are required to be the first line
         System.out.println("Cat parameterized constructor called!");
         this.numberOfLives = numberOfLives;
         this.breed = breed;
         this.hasFur = hasFur;
     }
 
-    @Override //overriden because animal also declares a method with the same name
-    public int getNumberOfLives() {
-        return numberOfLives;
+    public int getAnimalNumberOfLives() {
+        return super.numberOfLives;
     }
+
+    @Override //overridden because animal also declares a method with the same name
+    public int getNumberOfLives() {
+        return this.numberOfLives; // returns the cat number of lives, if we called super.numberOfLives it weuld
+    }
+
+
 
     public void setNumberOfLives(int numberOfLives) {
         this.numberOfLives = numberOfLives;
