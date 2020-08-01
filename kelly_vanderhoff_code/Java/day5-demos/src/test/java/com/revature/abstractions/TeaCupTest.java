7e8db2c100998ea6fc089f84c3b2f0cc1642fa96
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-07-31 20:30:24 -0500

    Added test messages to each TeaCup test

--- kelly_vanderhoff_code/Java/day5-demos/src/test/java/com/revature/abstractions/TeaCupTest.java
+++ kelly_vanderhoff_code/Java/day5-demos/src/test/java/com/revature/abstractions/TeaCupTest.java
@@ -10,60 +10,60 @@ public class TeaCupTest {
 
     @Test
     public void test1() {
-        assertNotNull("My cup should exist.", myCup);
+        assertNotNull("myCup should exist", myCup);
     }
 
     @Test
     public void test2() {
-        assertTrue("There is nothing in the cup", myCup.isEmpty());
+        assertTrue("myCup should be empty when made with the default parameters", myCup.isEmpty());
     }
 
     @Test
     public void test3() {
         myCup.fill("tea");
-        assertFalse("The cup is filled with tea", myCup.isEmpty());
+        assertFalse("myCup should not be empty when you fill it with tea", myCup.isEmpty());
     }
 
     @Test
     public void test4() {
         myCup.fill("tea");
-        assertEquals("tea", myCup.getDrink());
+        assertEquals("myCup should contain tea when you fill it with tea", "tea", myCup.getDrink());
     }
 
     @Test
     public void test5() {
-        assertTrue("Default tea cups have handles", myCup.hasHandle());
+        assertTrue("myCup made with the default parameters should have handles", myCup.hasHandle());
     }
 
     @Test
     public void test6() {
-        assertEquals(8, myCup.size());
+        assertEquals("myCup made with the default parameters should hold 8 ounces", 8, myCup.size());
     }
 
     @Test
     public void test7() {
         myCup.drink();
-        assertTrue(myCup.isEmpty());
+        assertTrue("myCup should be empty when you drink it dry", myCup.isEmpty());
     }
 
     @Test
     public void test8() {
         myCup.fill("coffee");
-        assertEquals("coffee", myCup.getDrink());
+        assertEquals("myCup should contain coffee when you fill it with coffee", "coffee", myCup.getDrink());
     }
 
     @Test
     public void test9() {
         myCup.fill("coffee");
         myCup.drink(6);
-        assertFalse(myCup.isEmpty());
+        assertFalse("myCup should not be empty when you only drink 6 ounces", myCup.isEmpty());
     }
 
     @Test
     public void test10() {
         myCup.fill("coffee");
         myCup.drink(6);
-        assertEquals(2,myCup.getRemainder());
+        assertEquals("myCup should still have 2 ounces left when you only drink 6", 2,myCup.getRemainder());
     }
 
     @Test
@@ -71,6 +71,27 @@ public class TeaCupTest {
         myCup.fill("coffee");
         myCup.drink(6);
         myCup.drink();
-        assertTrue(myCup.isEmpty());
+        assertTrue("myCup should be empty after you finish the dregs", myCup.isEmpty());
+    }
+
+    @Test
+    public void test12() {
+        myCup.fill("milk");
+        myCup.drink(10);
+        assertTrue("myCup should be empty even though there was less than you wanted to drink", myCup.isEmpty());
+    }
+
+    @Test
+    public void test13() {
+        TeaCup cup2 = myCup;
+        assertSame("cup2 and myCup should be the same TeaCup when myCup is assigned to cup2", myCup, cup2);
+        myCup = new TeaCup();
+    }
+
+    @Test
+    public void test14() {
+        TeaCup cup2 = myCup;
+        myCup = new TeaCup();
+        assertNotSame("cup2 and myCup should not be the same after assigning myCup to a new TeaCup", cup2, myCup);
     }
 }
