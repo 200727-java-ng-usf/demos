5283db76c607c2940337c39863381c0e4a6c954b
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-08-03 16:30:01 -0500

    Added scope demo files

--- kelly_vanderhoff_code/Java/fun-with-matt/src/main/java/com/revature/scope/Driver.java
+++ kelly_vanderhoff_code/Java/fun-with-matt/src/main/java/com/revature/scope/Driver.java
@@ -1,2 +1,47 @@
-package com.revature.scope;public class Driver {
+package com.revature.scope;
+
+public class Driver {
+
+    static int myInt = 5;
+
+    public static void main(String[] args) {
+
+        Human h1 = new Human(); // instance of human
+        h1.setName("Bill");
+        System.out.println(h1);
+
+        Human h2 = new Human("Matt", 34, 555555);
+        System.out.println(h2);
+
+        Human h3 = new Human("Sean", 20, 300);
+        h3.setHomePlanet("Mars");
+        System.out.println(h3);
+        System.out.println(h1);
+        System.out.println(h2);
+
+        myMethod(myInt + 6);
+
+        // ...other logic... i=?  // i DNE, waiting for GC
+
+        showBlockScope();
+    }
+
+    static void myMethod(int i) {
+        System.out.println(myInt);
+        System.out.println(i);
+    }
+
+    public static void showBlockScope() {
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            i = i + j;
+            j++;
+            System.out.println(i);
+        }
+//        j = j + i; // i is not defined beyond the scope of the for loop code block
+        System.out.println("Outside for loop: " + j);
+    }
 }
+
+// From some other class:
+// Driver.myMethod(42);
--- kelly_vanderhoff_code/Java/fun-with-matt/src/main/java/com/revature/scope/Human.java
+++ kelly_vanderhoff_code/Java/fun-with-matt/src/main/java/com/revature/scope/Human.java
@@ -1,2 +1,85 @@
-package com.revature.scope;public class Human {
+package com.revature.scope;
+
+public class Human {
+    /* Global Scope? No!
+        everything must belong to a class
+        Lifetime of a variable
+         - Static or Class
+            + Accessible to all members of a class
+         - Method or Local
+            + Accessible within the method in which it is declared
+         - Block
+            + Accessible inside of a block or control statement
+         - Instance or Object
+            + Accessible inside of that specific object
+
+        Shadowing
+          what occurs when variables of different scope have the same name (identifier)
+
+
+         What defines scope?  {...}
+     */
+
+    private String name;
+    private int age;
+    private int weight;
+    private static String homePlanet = "Earth";
+
+    // no-args constructor
+    public Human() {
+        // super(); // implicitly called
+    }
+
+    // parameterized constructor
+    // constructor w/ fields
+    public Human(String name, int age, int weight) {
+        this();
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    // accessors and mutators (getters and setters)
+    public String getName() {
+        return name;
+    }
+
+    // this is an example of shadowing vvv
+    public void setName(String name) {// name is method scope
+        this.name = name; // this.name refers to the specific instance's name field
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getHomePlanet() {
+        return homePlanet;
+    }
+
+    public void setHomePlanet(String homePlanet) {
+        this.homePlanet = homePlanet;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", homePlanet=" + homePlanet +
+                '}';
+    }
 }
