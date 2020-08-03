cebc06719cf8c26828e7cb426656264c749ac837
carlos arroyo <coarroyo30@gmail.com>
2020-08-03 17:34:27 -0400

    committing Generics and scopes to funwithmatt

---
+++ carlos_arroyo_code/java/day5-demos/src/main/java/com/revature/abstractions/Animal.java
@@ -0,0 +1,4 @@
+package com.revature.abstractions;
+
+public class Animal {
+}
---
+++ carlos_arroyo_code/java/funwithmatt/src/main/java/com/revature/scopes/Human.java
@@ -0,0 +1,89 @@
+package com.revature.scopes;
+
+/* Global Scope? nope!
+    everything must belong to a class.
+    Lifetime of a variable!
+    Types:
+    -Static or Class
+        -Accessible to all members of a class
+    - Method or Local
+        -Accessible within the method it is declared in
+    - Block
+        -Accessible inside of a block or control statement
+    - Instance
+        -Accessible inside of that specific object
+    -Shadowing
+        what occurs when variables in different scopes have the same name
+
+    What defines scope? {}
+ */
+public class Human {
+
+    private String name;
+    private int age;
+    private int weight;
+    static private String homePlanet = "Earth";
+
+    public String getHomePlanet() {
+        return homePlanet;
+    }
+
+    public void setHomePlanet(String homePlanet) {
+        this.homePlanet = homePlanet;
+    }
+
+    //no args constructor
+ /*   public Human() {
+
+        super();
+    }
+    */
+
+    //constructor w/fields
+    public Human (String name, int age, int weight) {
+        this.name=name;
+        this.age=age;
+        this.weight=weight;
+    }
+
+    public Human(){
+
+    }
+    // getters and setters (accessors and mutators)
+
+
+    public String getName() {
+        return name;
+    }
+
+    //Example of Shadowing
+    public void setName(String name) {//name is method scope
+        this.name = name; //this.name refers to specific instance's name field
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", homePlanet=" +homePlanet +
+                '}';
+    }
+}
