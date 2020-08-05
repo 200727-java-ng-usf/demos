d19963a56adbe31960ba34267e0a2c60f6a05144
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-05 18:06:54 -0400

    finished for the day

--- troy_davis_code/java/Week-b-demos/src/main/java/com/revature/collections/CollectionsDriver.java
+++ troy_davis_code/java/Week-b-demos/src/main/java/com/revature/collections/CollectionsDriver.java
@@ -29,7 +29,79 @@ public class CollectionsDriver {
         userSet.add(u3);
         userSet.add(u4);
         userSet.forEach(user -> System.out.println(user));
+        System.out.println("++-----------------------------++");
+        /*
+            Queue
+                - LinkedList
+                - follows the first-in first-out strategy
+                - adding to a Queue using .add is also known as enqueueing
+                -remove with queue . poll is also known as dequeuing
+                - members in the middle of the queue are inaccesable
+                -
+
+
+
+         */
+        Queue<User> userQueue = new LinkedList<>();
+        userQueue.add(u);
+        userQueue.add(u);
+        userQueue.add(new User("sdfsd","erwrw",5));
+        userQueue.add(null);
+
+        userQueue.forEach(user -> System.out.println(user));
+        System.out.println(userQueue);
+        System.out.println("++-----------------------------++");
+        User someUser = userQueue.poll();//poll removes the first value in the queue and returns it
+        System.out.println(someUser);
+        userQueue.forEach(user -> System.out.println(user));
+
+        System.out.println("++-----------------------------++");
+        User anotherUser = userQueue.peek();//peek returns next value but does not remove it
+        userQueue.forEach(user -> System.out.println(user));
+
+        /*
+            Deque - doubel ended queues
+            insert and removal from both ends
+         */
+        ArrayDeque<User> userDeque = new ArrayDeque<>();
+        userDeque.add(u);
+        userDeque.add(u2);
+
+        System.out.println("++-----------------------------++");
+        /*
+            Maps
+                - NOT A PART OF THE COLLECTIONS API
+                - "maps" a key to a value
+                - dictonary word is key, definition is value
+                - cannot have duplicate keys (one key can map to, at most, one value)
+                - in most implememntations, keys are allowed to be null - so long as there is only
+                one null key
+                - values do not need to be uniques
+         */
+        System.out.println("++-----------------------------++");
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("tmd1990us", u);
+        userMap.put(null, null);
+        userMap.put("tmd1990us", new User("erfwew","eedrqwe",5));
+        //userMap.put("tmd1990us", new User());
+        userMap.put(null, u2);
+
+        User thisUser = userMap.get("tmd1990us");
+        System.out.println(thisUser);
+
+        //you cannot iterate over a map, howvever
+        Set s = userMap.keySet();
+        for(String key : userMap.keySet()){
+            System.out.println(userMap.get(key));
+        }
+
+        Iterator<Map.Entry<String, User>> mapEntries = userMap.entrySet().iterator();
+        while (mapEntries.hasNext()){
+            System.out.println(mapEntries.next());
+        }
+
     }
+
 
 
 }
