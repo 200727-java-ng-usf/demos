c60d7c3c431c8cf3ad0dbb47df4b4d49cd8b7adb
capolimia <capolimia@gmail.com>
2020-08-06 15:32:33 -0400

    linked list and paird code

--- mia_capoli_code/Java/Generics/src/main/java/com/revature/structures/LinkedListDriver.java
+++ mia_capoli_code/Java/Generics/src/main/java/com/revature/structures/LinkedListDriver.java
@@ -7,20 +7,37 @@ public class LinkedListDriver {
     public static void main(String[] args) {
         CustomLinkedList<String> stringList = new CustomLinkedList<>();
         stringList.insert("test1");
+        stringList.insert("test3");
         stringList.insert("test2");
         stringList.insert("test3");
         stringList.insert("test4");
         stringList.insert("test5");
+        stringList.insert("test5");
         stringList.insert("test6");
+        stringList.insert("test3");
+        stringList.insert("test3");
+        stringList.insert("test9");
 
         stringList.printList();
 
         System.out.println("~~~~~~~~~~~~~~");
 
-        System.out.println(stringList.peek());
-        System.out.println(stringList.peek());
-        System.out.println(stringList.poll());
-        System.out.println(stringList.peek());
+        stringList.removeDuplicatesOf("test1");
+        stringList.printList();
+        System.out.println("~~~~~~~~~~~~~~");
+        stringList.removeDuplicatesOf("test3");
+        stringList.printList();
+        System.out.println("~~~~~~~~~~~~~~");
+        stringList.removeDuplicatesOf("test9");
+        stringList.printList();
+        System.out.println("~~~~~~~~~~~~~~");
+        stringList.removeDuplicatesOf("test10");
+        stringList.printList();
+//        System.out.println(stringList.peek());
+//        System.out.println(stringList.peek());
+//        System.out.println(stringList.poll());
+//        System.out.println(stringList.peek());
+
 
 
     }
--- mia_capoli_code/Java/Generics/src/main/java/com/revature/structures/util/CustomLinkedList.java
+++ mia_capoli_code/Java/Generics/src/main/java/com/revature/structures/util/CustomLinkedList.java
@@ -4,6 +4,7 @@ package com.revature.structures.util;
  * This is a custom implementation of a linked list data
  * structure which vaugely mirrors the Java Collections API
  * implementation LinkedList.
+ *
  * @param <T> a parameterized type
  * @author Mia
  */
@@ -45,28 +46,36 @@ public class CustomLinkedList<T> {
         tail = newNode;
     }
 
-        /**
-         * returns, but doesnt remove the data of the first node in the list.
-         */
-        public T peek(){
+    /**
+     * returns, but doesnt remove the data of the first node in the list.
+     */
+    public T peek() {
 
-            if (head != null) {
-                return head.getData();
-            } else {
-                return null;
-            }
-
+        if (head != null) {
+            return head.getData();
+        } else {
+            return null;
         }
+
+    }
 
     /**
      * returns and removes the data of the first node in the list
      */
-    public T poll(){
+    public T poll() {
+
 
         if (head != null) {
-            T data = head.getData();
-            head = head.getNextNode();
-            return data;
+            if (head == tail) {
+                T data = head.getData();
+                head = null;
+                tail = null;
+                return data;
+            } else {
+                T data = head.getData();
+                head = head.getNextNode();
+                return data;
+            }
         } else {
             return null;
         }
@@ -74,10 +83,62 @@ public class CustomLinkedList<T> {
     }
 
     public void printList() {
-        Node <T> currentNode = this.head;
+        Node<T> currentNode = this.head;
         while (currentNode != null) {
-        System.out.println("Node value:" + currentNode.getData());
-        currentNode = currentNode.getNextNode();
+            System.out.println("Node value:" + currentNode.getData());
+            currentNode = currentNode.getNextNode();
+        }
+
+    }
+
+    /**
+     * Removes the first occurrence of the provided data value in the linked list
+     *
+     * @param dataValue
+     * @return a boolean value; true if a data node was removed and false if one was not
+     */
+    public boolean removeByKey(T dataValue) {
+        Node<T> currentNode = this.head;
+
+        if (currentNode.getData() == dataValue) {
+            this.poll();
+            return true;
+        }
+
+        while (currentNode != null) {
+            while (currentNode.getNextNode() != null) {
+                if (currentNode.getNextNode().getData() == dataValue) {
+                    currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+                    return true;
+                }
+                currentNode = currentNode.getNextNode();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Removes all duplicates of a provided data value - leaving only one node with that value
+     *
+     * @param dataValue
+     */
+    public void removeDuplicatesOf(T dataValue) {
+        Node<T> currentNode = this.head;
+        int intCounter = 0;
+        if (currentNode.getData() == dataValue) {
+            intCounter++;
+        }
+
+        while (currentNode != null) {
+            if (currentNode.getNextNode() != null) {
+                if (currentNode.getNextNode().getData() == dataValue) {
+                    intCounter++;
+                    if (intCounter > 1 || currentNode.getNextNode().getNextNode() != null) {
+                        currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+                    }
+                }
+            }
+            currentNode = currentNode.getNextNode();
         }
 
     }
