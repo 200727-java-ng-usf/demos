aa6bff38ddef5bab0810884ce511410d5b16e3a6
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-06 10:04:14 -0700

    How linkedLists work

--- jason_hernandez_code/java/week-b-demos/src/main/java/com/revature/structures/LinkedListDriver.java
+++ jason_hernandez_code/java/week-b-demos/src/main/java/com/revature/structures/LinkedListDriver.java
@@ -1,7 +1,21 @@
 package com.revature.structures;
 
+import com.revature.structures.util.CustomLinkedList;
+
 public class LinkedListDriver {
     public static void main(String[] args) {
+        CustomLinkedList<String> stringList = new CustomLinkedList<>();
+        stringList.insert("test1");
+        stringList.insert("test2");
+        stringList.insert("test3");
+        stringList.insert("test4");
 
+        stringList.printList();
+
+        System.out.println("+--------------------------+");
+        System.out.println(stringList.peek()); // Looks but does not remove
+        System.out.println(stringList.peek()); // Same as before, because not removed
+        System.out.println(stringList.poll()); // Still same as before, but removed this time
+        System.out.println(stringList.poll()); // Now the next one
     }
 }
--- jason_hernandez_code/java/week-b-demos/src/main/java/com/revature/structures/util/CustomLinkedList.java
+++ jason_hernandez_code/java/week-b-demos/src/main/java/com/revature/structures/util/CustomLinkedList.java
@@ -5,5 +5,35 @@ public class CustomLinkedList<T> {
     private Node<T> head;
     private Node<T> tail;
 
+    public void insert(T data) {
+        Node<T> newNode = new Node<>(data, null);
+        if(head == null) {
+            System.out.println("List is empty; adding first node");
+            head = newNode;
+        } else {
+            System.out.println("List has existing elements; adding new node to the end");
+            tail.setNextNode(newNode);
 
+        }tail = newNode;
+    }
+    public T peek () {
+        if(head != null) {
+            return head.getData();
+        }
+        return null;
+    }
+    public T poll () {
+        if(head != null) {
+            T data = head.getData();
+            head = head.getNextNode();
+            return data;
+        }
+        return null;
+    }
+
+    public void printList() {
+        Node<T> currentNode = this.head;
+        System.out.println("Current node: " + currentNode);
+        currentNode = currentNode.getNextNode();
+    }
 }
