6e8e876c6d8f9f96fb74a971449167ddc9a3dfed
SeanRog <seanmikerog@gmail.com>
2020-08-06 08:08:18 -0700

    created Car.java, Box.java, ComparableDriver.java and ComparatorDriver.java. So far we've demonstrated the use of the Comparable interface

---
+++ sean_rogers_code/Java/week-b-demos/src/main/java/com/revature/compare/ComparableDriver.java
@@ -0,0 +1,41 @@
+package com.revature.compare;
+
+
+import com.revature.compare.models.Box;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+/*
+    Comparable
+
+        -one method: int compareTo(T obj)
+        -functional interfaces (one abstract method); can be used with lambdas
+        -utility classes such as Arrays and Collections have methods that take in Comparable types
+        -uses a parameterized type (generics)
+        -returns an int value which is either negative, zero, or positive (less than, equal to, or greater than)
+ */
+public class ComparableDriver {
+    public static void main(String[] args) {
+
+        Box myBox = new Box(50.0, "brown");
+        Box yourBox = new Box(45.5, "white");
+
+        Box[] ourBoxes = {myBox, new Box(3.5, "red"), yourBox};
+
+        for(Box box : ourBoxes) {
+            System.out.println(box);
+        }
+
+        System.out.println("+--------------------------+");
+
+        //
+        Arrays.sort(ourBoxes);
+        for(Box box : ourBoxes) {
+            System.out.println(box);
+        }
+
+
+
+    }
+}
---
+++ sean_rogers_code/Java/week-b-demos/src/main/java/com/revature/compare/ComparatorDriver.java
@@ -0,0 +1,11 @@
+package com.revature.compare;
+
+/*
+    Comparator
+        -one method: int compare(T obj1, T obj2)
+        -function interface
+        -returns an int value which is either negative, zero, or positive (less than, equal to, or greater than)
+ */
+public class ComparatorDriver {
+
+}
