d9faea97ee40a024f69b42ed410e169437fc1102
Stephen Brasel <wearyroad@gmail.com>
2020-09-14 15:34:06 -0400

    quiz up and running

--- web/JS-QUIZ/js/js-quiz.js
+++ web/JS-QUIZ/js/js-quiz.js
@@ -29,14 +29,86 @@ window.onload = () => {
 	submitBtn.addEventListener('click', showResults);
 }
 
-function buildQuiz(){
+async function buildQuiz() {
+
+	let getQuestionsFunction = await getQuestions();
+	let myQuestions = await getQuestionsFunction();
+	console.log(myQuestions);
+
+	//Convenience reference for our quiz containre
+	let quizContainer = document.getElementById('quiz-container');
+
+	//Create an array which can hold the html of our quiz.
+	const htmlOutput = [];
+
+	/*
+		Next, we can start building the html for each question. We will 
+		need to loop through the questions array. 
+	*/
+
+	myQuestions.forEach((currentQuestion, questionNumber) =>{
+
+		// create an array for storing the list of answer choices
+		const answers = [];
+
+		//for each available answer create some HTML that will be rendered on the screen 
+		for(let letter in currentQuestion.answers){
+			answers.push(`
+			<label>
+				<input type="radio" name="question-${questionNumber}" value="${letter}" />
+				${letter}: ${currentQuestion.answers[letter]}
+			</label>
+			`);
+		}
+
+		//create the HTML for this question 
+		htmlOutput.push(`
+		<br/>
+		<div class="questions">${currentQuestion.question}</div>
+		<div class="answers">${answers.join('')}</div>
+		`);
+
+	});
+
+	quizContainer.innerHTML = htmlOutput.join('');
+}
+
+function isQuizValid() {
 
 }
 
-function isQuizValid(){
+function showResults() {
 
 }
 
-function showResults(){
+//async await introduced in ES7.
+async function getQuestions() {
+
+	let myQuestions;
+
+	return async function () {
+		if (myQuestions) {
+			console.log('questions already retrieved, returning existing questions.');
+			return myQuestions;
+		} else {
+			console.log('first time request for questions.');
+			let response = await fetch('https://api.jsonbin.io/b/5f5fb889302a837e95663c95', {
+				method: 'GET',
+				headers: {
+					'secret-key': '$2b$10$5ByNoV2Moynb1rjMK0dbnuOC0vFUq.N34QN0MWRT8VTlCCyRCutE6'
+				}
+			});
+
+			myQuestions = await response.json();
+
+			// response.then(resp => {
+			// 	let bodyDataPromise = resp.json();
+			// 	myQuestions = bodyDataPromise.then();
+			// }).catch(err =>{
+			// 	console.error(err);
+			// })
+			return myQuestions;
+		}
+	}
 
 }
