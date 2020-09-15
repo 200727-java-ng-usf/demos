fecd19802b11511774b7d876a139622deac2ed55
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-09-15 15:43:43 -0400

    updates

--- troy_davis_code/node.js/angular/hello-ng/src/app/app.module.ts
+++ troy_davis_code/node.js/angular/hello-ng/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
 import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorsComponent,
-    FirstComponentComponent,
     FirstComponent
   ],
   imports: [
