65b4b327216775f0301b6dea27993db747474fdb
Stephen Brasel <wearyroad@gmail.com>
2020-09-23 16:02:02 -0400

    Added prop values to app.config, still need to make beans

--- spring/week6demo/spring-quizzard/src/main/java/com/revature/quizzard/AppConfig.java
+++ spring/week6demo/spring-quizzard/src/main/java/com/revature/quizzard/AppConfig.java
@@ -1,4 +1,30 @@
 package com.revature.quizzard;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan
+@EnableTransactionManagement
+@PropertySource("classpath:app.properties")
 public class AppConfig {
+
+	@Value("${db.driver}")
+	private String dbDriver;
+
+	@Value("${db.url}")
+	private String dbUrl;
+
+	@Value("${db.schema}")
+	private String dbSchema;
+
+	@Value("${db.username}")
+	private String dbUsername;
+
+	@Value("${db.password}")
+	private String password;
+
 }
