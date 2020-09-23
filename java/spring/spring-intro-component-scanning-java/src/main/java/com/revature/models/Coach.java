bac3837239a2c010c23d0a8c86c5648217ec2703
wezley <wezley.singleton@gmail.com>
2020-09-23 11:25:23 -0400

    gitignore

---
+++ java/spring/spring-intro-component-scanning-java/src/main/java/com/revature/models/Coach.java
@@ -0,0 +1,11 @@
+package revature.models;
+
+import com.revature.services.MotivationService;
+
+public interface Coach {
+
+    String getDailyWorkout();
+    String getMotivation();
+    MotivationService getMotivationService();
+    
+}
