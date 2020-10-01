ea41929661e100ad88b08f578eedea9e3e6de201
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-10-01 12:18:00 -0400

    catch up

---
+++ java/web-services/msa-demos/user-service/src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,30 @@
+package com.revature.services;
+
+import com.revature.models.AppUser;
+import com.revature.repos.UserRepository;
+import com.revature.web.dtos.AppUserDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepo;
+
+    private UserService(UserRepository repo) {
+        this.userRepo = repo;
+    }
+
+    @Transactional(readOnly = true)
+    public List<AppUserDTO> getAllUsers() {
+        return ((List<AppUser>) userRepo.findAll())
+                .stream()
+                .map(appUser -> new AppUserDTO(appUser))
+                .collect(Collectors.toList());
+    }
+
+
+}
