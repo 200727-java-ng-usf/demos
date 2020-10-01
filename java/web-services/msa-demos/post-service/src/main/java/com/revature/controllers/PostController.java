9106718c82fba6ae6ca7945e6c2025d7b75f475e
wezley <wezley.singleton@gmail.com>
2020-10-01 13:01:24 -0400

    push basic post service

---
+++ java/web-services/msa-demos/post-service/src/main/java/com/revature/controllers/PostController.java
@@ -0,0 +1,39 @@
+package com.revature.controllers;
+
+import com.revature.models.Post;
+import com.revature.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService service) {
+        this.postService = service;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping(value = "/poster/{posterId}")
+    public List<Post> getPostsByPosterId(@PathVariable int posterId) {
+        return postService.getPostsByPosterId(posterId);
+    }
+
+    @GetMapping(value = "/id/{postId}")
+    public Post getPostById(@PathVariable int postId) {
+        return postService.getPostById(postId);
+    }
+
+}
