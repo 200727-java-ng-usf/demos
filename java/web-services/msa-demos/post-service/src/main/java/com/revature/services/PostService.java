f5ed487dce49fbbd5eb8731128f1df96bfa8122e
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-10-02 12:15:13 -0400

    push before pull

---
+++ java/web-services/msa-demos/post-service/src/main/java/com/revature/services/PostService.java
@@ -0,0 +1,34 @@
+package com.revature.services;
+
+import com.revature.models.Post;
+import com.revature.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private PostRepository postRepo;
+
+    @Autowired
+    public PostService(PostRepository repo) {
+        this.postRepo = repo;
+    }
+
+    @Transactional
+    public List<Post> getAllPosts() {
+        return (List<Post>) postRepo.findAll();
+    }
+
+    @Transactional
+    public List<Post> getPostsByPosterId(int posterId) {
+        return postRepo.findPostsByPosterId(posterId);
+    }
+
+    public Post getPostById(int postId) {
+        return postRepo.findById(postId).orElseThrow(() -> new RuntimeException("No post found with provided id."));
+    }
+}
---
+++ java/web-services/msa-demos/post-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into posts (title, body, poster_id)
+values ('First post!', 'Hello everyone! This is the first post!', 1);
+
+insert into posts (title, body, poster_id)
+values ('So cool!', 'Another post', 3);
+
+insert into posts (title, body, poster_id)
+values ('This is great!', 'Spring Data and Spring Boot might put me out of a job one day!', 2);
+
+insert into posts (title, body, poster_id)
+values ('Hopefully not', 'Nah, someone will always have to build some part of the app...', 3);
